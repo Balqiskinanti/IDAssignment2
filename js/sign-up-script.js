293fbf17f5930ae339ab699094b3c487370e78fa
Balqis Kinanti <S10204973@connect.np.edu.sg>
2020-12-24 07:50:17 +0800

    add api to valideate email, add signup to storage

--- js/sign-up-script.js
+++ js/sign-up-script.js
@@ -16,5 +16,28 @@ $(document).ready(function () {
         }else{
             count=0;
         }
+
+        // api  to check if email is valid
+        var settings = {
+            "url": "https://verify.gmass.co/verify?email="+getMail+"&key=85df4d70-399f-473c-9c2e-5c8ac9114ed6",
+            "method": "GET",
+            "timeout": 0,
+            "headers": {
+            },
+        };
+        $.ajax(settings).done(function (response) {
+            let s= new signUp(getName,getMail,getPassword,getAddress);
+            let signUpArray =[];
+
+            if (localStorage.getItem('signUp')) {
+                signUpArray = JSON.parse(localStorage.getItem("signUp"));
+            }
+
+            signUpArray.push(s);
+
+            localStorage["signUp"]=JSON.stringify(signUpArray);
+            alert("Sign up successful. You can log in now");
+            $("#myForm")[0].reset()
+        })
     })
 })
