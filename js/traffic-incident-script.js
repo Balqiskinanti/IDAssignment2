a82184810c962016b4ac5c53b42b6e72ad5af1fa
Balqis Kinanti <S10204973@connect.np.edu.sg>
2020-12-24 10:56:02 +0800

    get user input for search, loop and retrieve data

--- js/traffic-incident-script.js
+++ js/traffic-incident-script.js
@@ -13,6 +13,20 @@ $(document).ready(function(){
         data: {
         },
         success:function(data){
+            getTrafficIncidentsInput = $("#trafficIncidentsInput").val();
+    
+            $("#trafficIncidentsInput").change(function (){
+                getTrafficIncidentsInput = $("#trafficIncidentsInput").val();
+            })
+
+            $(".btn").click(function (e){
+                e.preventDefault();
+                for (i = 0; i < data.value.length; i++) {
+                    if (data.value[i].Message.toLowerCase().indexOf(getTrafficIncidentsInput.toLowerCase()) != -1){
+                        console.log(`${data.value[i].Type} ${data.value[i].Message}`);
+                    }
+                }
+            })
         }
     })
 })
