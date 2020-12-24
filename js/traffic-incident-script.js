a765d2ec8c2dead0960a74b11d5f9dc48c362c84
Balqis Kinanti <S10204973@connect.np.edu.sg>
2020-12-24 10:59:27 +0800

    append message of incidents to html page

--- js/traffic-incident-script.js
+++ js/traffic-incident-script.js
@@ -13,6 +13,29 @@ $(document).ready(function(){
         data: {
         },
         success:function(data){
+            for (i = 0; i < data.value.length; i++) {
+                if(data.value[i].Type == "Accident"){
+                    $(".accident").append("<br>"+data.value[i].Message).show();
+                }else if(data.value[i].Type == "Roadwork"){
+                    $(".roadwork").append("<br>"+data.value[i].Message).show();
+                }else if(data.value[i].Type == "Vehicle breakdown"){
+                    $(".breakdown").append("<br>"+data.value[i].Message).show();
+                }else if(data.value[i].Type == "Weather"){
+                    $(".weather").append("<br>"+data.value[i].Message).show();
+                }else if(data.value[i].Type == "Obstacle"){
+                    $(".obstacle").append("<br>"+data.value[i].Message).show();
+                }else if(data.value[i].Type == "Road Block"){
+                    $(".roadblock").append("<br>"+data.value[i].Message).show();
+                }else if(data.value[i].Type == "Heavy Traffic"){
+                    $(".traffic").append("<br>"+data.value[i].Message).show();
+                }else if(data.value[i].Type == "Misc."){
+                    $(".misc").append("<br>"+data.value[i].Message).show();
+                }else if(data.value[i].Type == "Diversion"){
+                    $(".diversion").append("<br>"+data.value[i].Message).show();
+                }else if(data.value[i].Type == "Unattended Vehicle"){
+                    $(".vehicle").append("<br>"+data.value[i].Message).show();
+                }
+            }
             getTrafficIncidentsInput = $("#trafficIncidentsInput").val();
     
             $("#trafficIncidentsInput").change(function (){
