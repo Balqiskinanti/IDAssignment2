2efc60e6dc24952b9f08366c992e76da1574a789
Balqis Kinanti <S10204973@connect.np.edu.sg>
2020-12-24 08:19:11 +0800

    check if login = signup. add login to localstorage

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     // parse signup json to loop and check if user has sign up
     signUpArray = JSON.parse(localStorage.getItem("signUp"));
-    
+
     // get user input
     var getMail = $("#mailInput").val();
     var getPassword = $("#passwordInput").val();
@@ -13,4 +13,32 @@ $(document).ready(function () {
     $("#passwordInput").change(function (){
         getPassword = $("#passwordInput").val();
     })
+
+    $("#btn").click(function (e) {
+        flag = false;
+        // loop sign up array to check if user sign up
+        for (i = 0; i < signUpArray.length; i++) {
+            // error checking 1: user to login with matching username and password as the one they've sign up
+            // pass error checking. add to local storage
+            if (signUpArray[i].email == getMail && signUpArray[i].password == getPassword){
+                flag = true;
+                let l= new login(signUpArray[i].name,signUpArray[i].email,signUpArray[i].password,signUpArray[i].address);
+                let loginArray =[];
+                loginArray.push(l);
+                localStorage["login"]=JSON.stringify(loginArray);
+            }
+        }
+        if(flag != true){
+            e.preventDefault();
+            alert("wrong password");
+        }
+    })
+
+    // function object constructor for login
+    function login(name,email,password,address){
+        this.name=name;
+        this.email=email;
+        this.password=password;
+        this.address=address;
+    }
 })
