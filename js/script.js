36ede79800a7eafec822ad539a08c2717c39de1e
Balqis Kinanti <S10204973@connect.np.edu.sg>
2020-12-27 01:43:57 +0800

    add error checking for when user hasnt sign up

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-$(document).ready(function () {
+$(document).ready(function (e) {
     // parse signup json to loop and check if user has sign up
     signUpArray = JSON.parse(localStorage.getItem("signUp"));
-
     // get user input
     var getMail = $("#mailInput").val();
     var getPassword = $("#passwordInput").val();
@@ -17,20 +16,36 @@ $(document).ready(function () {
     $("#btn").click(function (e) {
         flag = false;
         // loop sign up array to check if user sign up
-        for (i = 0; i < signUpArray.length; i++) {
-            // error checking 1: user to login with matching username and password as the one they've sign up
-            // pass error checking. add to local storage
-            if (signUpArray[i].email == getMail && signUpArray[i].password == getPassword){
-                flag = true;
-                let l= new login(signUpArray[i].name,signUpArray[i].email,signUpArray[i].password,signUpArray[i].address);
-                let loginArray =[];
-                loginArray.push(l);
-                localStorage["login"]=JSON.stringify(loginArray);
-            }
-        }
-        if(flag != true){
+        if(signUpArray == null){
             e.preventDefault();
-            alert("wrong password");
+            alert("please sign up");
+        }else{
+            for (i = 0; i < signUpArray.length; i++) {
+                // error checking 1: user to login with matching username and password as the one they've sign up
+                // pass error checking. add to local storage
+                if (signUpArray[i].email == getMail && signUpArray[i].password == getPassword){
+                    flag = true;
+                    let l= new login(signUpArray[i].name,signUpArray[i].email,signUpArray[i].password,signUpArray[i].address);
+                    let loginArray =[];
+                    loginArray.push(l);
+                    localStorage["login"]=JSON.stringify(loginArray);
+
+                
+                    let s= new savedData(signUpArray[i].name,signUpArray[i].email,signUpArray[i].password,signUpArray[i].address);
+                    let savedDataArray =[];
+                    if (localStorage.getItem('savedData')) {
+                        savedDataArray = JSON.parse(localStorage.getItem("savedData"));
+                    }
+                    savedDataArray.push(s);
+                    localStorage["savedData"]=JSON.stringify(savedDataArray);
+
+                }
+            }
+            if(flag != true){
+                e.preventDefault();
+                alert("wrong password");
+            }
+
         }
     })
 
@@ -41,4 +56,12 @@ $(document).ready(function () {
         this.password=password;
         this.address=address;
     }
+
+    function savedData(name,email,password,address){
+        this.name=name;
+        this.email=email;
+        this.password=password;
+        this.address=address;
+    }
+
 })
